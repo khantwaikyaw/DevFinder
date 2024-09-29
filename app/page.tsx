2b02e9d9ebd0c8eb19bfe212de26633b9ff9d14a
Khant Wai Kyaw <khantwaikyaw11@gmail.com>
2024-09-30 01:59:54 +0700

    refactor: fix incorrect component directory name
    - update directory name componets to components
    - update app/page for imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-import DevFinder from '@/componets/DevFinder/DevFinder'
-import styles from './page.module.css'
+import DevFinder from "@/components/DevFinder/DevFinder";
+import styles from "./page.module.css";
 
 const DevFinderHome = () => {
   return (
     <main className={styles.main}>
       <DevFinder />
     </main>
-  )
-}
+  );
+};
 
-export default DevFinderHome
+export default DevFinderHome;
